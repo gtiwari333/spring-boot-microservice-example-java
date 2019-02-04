9a85e2d1bdde5a6faa3cb55f0e21738c15e20f3a
Ganesh Tiwari <gtiwari@sdata.us>
2019-02-04 01:11:34 -0600

    cleanup

--- greeting-service/src/main/java/g/t/greeting/GreetingServiceApplication.java
+++ greeting-service/src/main/java/g/t/greeting/GreetingServiceApplication.java
@@ -3,14 +3,12 @@ package g.t.greeting;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@RibbonClient("greetingService")
 public class GreetingServiceApplication {
 
     public static void main(String[] args) {
--- time-service/src/main/java/g/t/time/TimeServiceApplication.java
+++ time-service/src/main/java/g/t/time/TimeServiceApplication.java
@@ -3,7 +3,6 @@ package g.t.time;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +12,6 @@ import java.util.Map;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@RibbonClient("timeService")
 public class TimeServiceApplication {
 
     public static void main(String[] args) {
