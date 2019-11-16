11a6e88518c8e74e54335c599694bbe4e796b596
Ganesh <gtiwari333@gmail.com>
2019-11-16 17:21:31 -0600

    circuit breaker, fallbacks, hystrix dashboard and actuators

--- gateway-app/src/main/java/g/t/gatewayapp/TestGatewayApplication.java
+++ gateway-app/src/main/java/g/t/gatewayapp/TestGatewayApplication.java
@@ -1,22 +1,29 @@
 package g.t.gatewayapp;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableFeignClients
+@EnableCircuitBreaker
 public class TestGatewayApplication {
 
     public static void main(String[] args) {
@@ -26,17 +33,17 @@ public class TestGatewayApplication {
     @Bean
     @LoadBalanced
         //this will search in the registry
-    RestTemplate restTemplate() {
-        return new RestTemplate();
+    RestTemplate restTemplate(RestTemplateBuilder rtb) {
+        return rtb.build();
     }
 
     @RestController
     static class GatewayAppController {
-        private final RestTemplate restTemplate;
+        private final GreetingService greetingService;
         private final TimeService timeService;
 
-        GatewayAppController(RestTemplate restTemplate, TimeService timeService) {
-            this.restTemplate = restTemplate;
+        GatewayAppController(GreetingService greetingService, TimeService timeService) {
+            this.greetingService = greetingService;
             this.timeService = timeService;
         }
 
@@ -45,17 +52,48 @@ public class TestGatewayApplication {
             Map<String, String> resp = timeService.getTime();
             String time = resp.get("servertime");
 
-            String greeting = restTemplate.getForObject("http://greeting-service/api/greeting/", String.class);
+            String greeting = greetingService.getGreeting();
 
             return "The server says : " + greeting + ". Server time is " + time;
         }
+
+
     }
 }
 
-@FeignClient(value = "time-service")
+@Service
+class GreetingService {
+    private final RestTemplate restTemplate;
+
+    GreetingService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @HystrixCommand(fallbackMethod = "getDefaultGreeting")
+    String getGreeting() {
+        return restTemplate.getForObject("http://greeting-service/api/greeting/", String.class);
+    }
+
+    private String getDefaultGreeting() {
+        return "Hello world";
+    }
+
+}
+
+@FeignClient(value = "time-service", fallback = HystrixFallbackTimeService.class)
 interface TimeService {
 
     @GetMapping({"/api/time/"})
     Map<String, String> getTime();
 
+}
+
+@Component
+        //it won't be the primary bean
+class HystrixFallbackTimeService implements TimeService {
+
+    @Override
+    public Map<String, String> getTime() {
+        return Map.of("servertime", "Fallback to" + LocalDateTime.now().toString());
+    }
 }
